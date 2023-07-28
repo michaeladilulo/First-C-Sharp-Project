9cbd0252a0db5daac020080e6d79ede804ad0467
michaeladilulo <michaeladilulo@gmail.com>
2023-07-27 23:10:16 -0600

    Conditionals

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -2,18 +2,23 @@
 {
     class Program
     {
-        public static void Main(String[] aruments)
+        public static void Main(String[] arguments)
         {
-            int firstNumber = 5;
-            float secondNumber = 2.4f;
-            string firstString = "Hello World";
-            char firstChar = 'a';
-            bool isTrue = false;
-            System.Console.WriteLine(firstNumber);
-            System.Console.WriteLine(secondNumber);
-            System.Console.WriteLine(firstString);
-            System.Console.WriteLine(firstChar);
-            System.Console.WriteLine(isTrue);
+            bool complete = false;
+            bool started = true;
+
+            if(complete)
+            {
+                int x = 5;
+            }
+            else if(started)
+            {
+                int x = 4;
+            }
+            else
+            {
+                int x = 2;
+            }
         }
     }
 }
