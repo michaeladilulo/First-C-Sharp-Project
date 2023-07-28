65ea701a2c4cae72fcd5eef5344f967b2d91b85e
michaeladilulo <michaeladilulo@gmail.com>
2023-07-27 21:19:40 -0600

    Variable Examples

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -4,7 +4,16 @@
     {
         public static void Main(String[] aruments)
         {
-            System.Console.WriteLine("Hello World");
+            int firstNumber = 5;
+            float secondNumber = 2.4f;
+            string firstString = "Hello World";
+            char firstChar = 'a';
+            bool isTrue = false;
+            System.Console.WriteLine(firstNumber);
+            System.Console.WriteLine(secondNumber);
+            System.Console.WriteLine(firstString);
+            System.Console.WriteLine(firstChar);
+            System.Console.WriteLine(isTrue);
         }
     }
 }
