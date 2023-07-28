74fb39115a9e337b10d4906da561375fbc9b03a9
michaeladilulo <michaeladilulo@gmail.com>
2023-07-27 20:51:13 -0600

    added Main function

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace FirstProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is my first console app!");
+    }
+}
